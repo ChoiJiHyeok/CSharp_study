8aa70f22335dfc06a287197ebaec32a85db0ff7c
Choi Ji hyuk <arrouw12@gmail.com>
2024-02-26 13:31:37 +0900

    24.02.26 13:30

--- CSharpStudy/CSharpStudy/Program.cs
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
+using System.Threading;
 
 
 namespace CSharpStudy
 {
     class Program
     {
+       static object lockObj = new object();
+
         static void Main(string[] args)
         {
             int numItems = 64;
@@ -25,6 +28,42 @@ namespace CSharpStudy
             }
 
             Console.WriteLine("The square of 23 is {0} (should be {1})", cd[23], 23 * 23);
+
+            Thread thread1 = new Thread(() => FindNumber(cd, numItems));
+            Thread thread2 = new Thread(() => FindNumber(cd, numItems));
+            thread1.Start();
+            thread2.Start();
+
+            while(true)
+            {
+                if (thread1.ThreadState == ThreadState.Stopped && thread2.ThreadState == ThreadState.Stopped)
+                {
+                    Console.WriteLine($"{ cd[23].ToString()}, 스레드 동작 정지");
+                    break;
+                }
+            }
+
         }
+
+        private static void FindNumber(ConcurrentDictionary<int, int> cd, int numItems)
+        {
+            //lock(lockObj)
+            //{
+            //    for (int i = 0; i < numItems; i++)
+            //    {
+            //        if (i == 23)
+            //        {
+            //            cd[i] += 1;
+            //        }
+
+            //    }
+            //}
+            for(int i = 0; i < numItems; i++)
+            {
+                cd.AddOrUpdate(i, 23 * 23, (key, value) => value + 1);
+            }
+            Console.WriteLine(cd[23].ToString());            
+        }
+ 
     }
 }
