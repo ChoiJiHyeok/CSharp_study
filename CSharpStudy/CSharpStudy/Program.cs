19dd3137cf11aa4255bb97bd88939a5acbd0133b
Choi Ji hyuk <arrouw12@gmail.com>
2024-02-26 09:45:35 +0900

    24.02.26 09:;45

---
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+
+namespace CSharpStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numItems = 64;
+            int initialCapacity = 101;
+
+            int numProcs = Environment.ProcessorCount;
+            int concurrencyLevel = numProcs * 2;
+
+            ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>(concurrencyLevel, initialCapacity);
+            for (int i = 0; i < numItems; i++)
+            {
+                cd[i] = i * i;
+            }
+
+            Console.WriteLine("The square of 23 is {0} (should be {1})", cd[23], 23 * 23);
+        }
+    }
+}
